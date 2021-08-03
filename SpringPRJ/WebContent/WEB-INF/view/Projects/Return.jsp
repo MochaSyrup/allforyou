beb3b31b4cf076e1775804d31b69df238d9d284a
ohju96 <ohju96@naver.com>
2021-08-03 13:14:02 +0900

    스프링 파일 올리기

---
+++ SpringPRJ/WebContent/WEB-INF/view/Projects/Return.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Return</title>
+</head>
+<body>
+<form action="Project/Forgot.do">
+<script>
+
+alert('없는 아이디 입니다')
+
+</script>
+</form>
+</body>
+</html>
---
+++ SpringPRJ/src/poly/dto/ProjectsDTO.java
@@ -0,0 +1,56 @@
+package poly.dto;
+
+
+public class ProjectsDTO {
+	
+	private String user_id;
+	private String user_pwd;
+	private String user_name;
+	private String tomail;
+	private String title;
+	private String contents;
+	
+	
+	
+	public String getTomail() {
+		return tomail;
+	}
+	public void setTomail(String tomail) {
+		this.tomail = tomail;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getUser_pwd() {
+		return user_pwd;
+	}
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
+	}
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+	
+	
+}
